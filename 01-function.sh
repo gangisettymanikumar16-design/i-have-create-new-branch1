8b5e21405ab2542c0e22e0ea4cf8066d799de7b4
.email.com <gangisettymani98@gmail.com>
2026-04-12 21:31:14 +0530

    i would have create a new file and so it is deflut file

---
+++ 01-function.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+Red="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ...$R FAILURE"
+        exit 1
+    else
+        echo -e "$2 ... $g SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y
+    VALIDATE $? "Installing MySQL"
+else
+    echo "MySQL is already ... INSTALLED"
+fi
+
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? "Installing Git"
+else
+    echo "Git is already ... INSTALLED"
+fi
